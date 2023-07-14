e735a9cdb87e03f399fb5b0a09f63a6a9be96e0f
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-07-14 11:13:03 +0200

    Completato lo sviluppo del footer;

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,9 @@ function Footer() {
     return (
         <footer>
             <div className='ctnFooter'>
-
+                <p>
+                    Per approfondire&#58; <a href='#'>OpenAI Blog</a> &#8211; <a href='#'>ChatGPT</a> &#8211; <a href='#'>DALL-E</a>
+                </p>
             </div>
         </footer>
     );
